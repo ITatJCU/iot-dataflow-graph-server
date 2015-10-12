4b91af6199ace142a398e6d0b56b74481cbbf740
Jason Holdsworth <ozyjay@gmail.com>
2015-10-13 09:53:09 +1000

    adjusted the network connect to be a bit faster

--- real-devices/Arduino/ESP8266/ArduinoIDE Library/ESP8266MulticastUDP/ESP8266MulticastUDP.cpp
+++ real-devices/Arduino/ESP8266/ArduinoIDE Library/ESP8266MulticastUDP/ESP8266MulticastUDP.cpp
@@ -29,7 +29,7 @@ void joinWiFi(const String& ssid, const String& passcode)
 	WiFi.begin(ssid.c_str(), passcode.c_str());
 
 	int attempts = 0;
-	const int maxAttempts = 200;
+	const int maxAttempts = 100;
 	while (WiFi.status() != WL_CONNECTED) {
 		++attempts;
 
